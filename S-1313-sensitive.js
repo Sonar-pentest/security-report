b975075f74d8e7d8383069bf76a46947b4b55eb6
Mathieu Suen <mathieu.suen@sonarsource.com>
2022-12-07 15:41:20 +0100

    Sensitive PR

---
+++ S-1313-sensitive.js
@@ -0,0 +1,4 @@
+address = "192.168.12.42"; // Sensitive
+address = "2001:0db8:3c4d:0015:0000:0000:1a2f:1a2b"; // Sensitive
+address = "0012.0012.0012.0001"; // Sensitive
+address = "0a.0a.0a.01"; // Sensitive
